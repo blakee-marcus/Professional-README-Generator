2a927ca2f913c79793c0df089d54ca20fa66d387
bm <blake0marcus@gmail.com>
2022-05-07 15:18:47 -0700

    Reformat Functions

--- index.js
+++ index.js
@@ -1,94 +1,111 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generateReadMe = require('./src/readme-template.js');
 // TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project? (Required)',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your title!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter a description of your project (Required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a description!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Enter installation instructions for your project (Required)',
-            validate: installationInput => {
-                if (installationInput) {
-                    return true;
-                } else {
-                    console.log('Please enter installation instructions!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Enter usage for your project (Required)',
-            validate: usageInput => {
-                if (usageInput) {
-                    return true;
-                } else {
-                    console.log('Please enter usage for your project!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Enter contribution guidelines (Required)',
-            validate: contributionInput => {
-                if (contributionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter contribution guidelines!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Enter test instructions (Required)',
-            validate: testInput => {
-                if (testInput) {
-                    return true;
-                } else {
-                    console.log('Please enter contribution guidelines!');
-                    return false;
-                }
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your title!');
+                return false;
             }
         }
-    ]);
-};
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions for your project (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('Please enter installation instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage for your project (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter usage for your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter contribution guidelines!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license (Required)',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'Unlicense']
+        
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeFile(fileContent) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/README.md', fileContent, function(err) {
+           if (err) {
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File Created!'
+            }); 
+        });
+        
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
--- src/readme-template.js
+++ src/readme-template.js
@@ -1,38 +1,27 @@
 module.exports = templateData => {
+    const { title, description, installation, usage, contribution, test } = templateData;
+
     return `
         # ${title}
 
-        Foobar is a Python library for dealing with word pluralization.
+        ${description}
         
         ## Installation
         
-        Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
-        
-        \`\`\`bash
-        pip install foobar
-        \`\`\`
+        ${installation}
         
         ## Usage
         
-        \`\`\`python
-        import foobar
-        
-        # returns 'words'
-        foobar.pluralize('word')
-        
-        # returns 'geese'
-        foobar.pluralize('goose')
-        
-        # returns 'phenomenon'
-        foobar.singularize('phenomena')
-        \`\`\`
+        ${usage}
         
         ## Contributing
-        Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
         
-        Please make sure to update tests as appropriate.
+        ${contribution}
         
-        ## License
-        [MIT](https://choosealicense.com/licenses/mit/)
+        ## Test Instructions
+
+        ${test}
+
+        
     `
 }
